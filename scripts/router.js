d22c2315e693468e980a8ddf6e018c7ec10c7b68
Amanda Quach <amquach00@gmail.com>
2021-05-16 17:22:25 -0700

    finished lab

--- scripts/router.js
+++ scripts/router.js
@@ -1,38 +1,48 @@
 // router.js
 
 export const router = {};
+const body = document.querySelector('body');
+const title = document.querySelector('header h1');
 
 /**
  * Changes the "page" (state) that your SPA app is currently set to
  */
-router.setState = function() {
-  /**
-   * - There are three states that your SPA app will have
-   *    1. The home page
-   *    2. The entry page (showing one individual entry)
-   *    3. The settings page (currently blank, no actual settings here, just a placeholder where a real settings page would go)
-   * 
-   * - If you look at the CSS, we have 2 classes you can add to the body element to help change states, "settings" and "single-entry"
-   * - Changing states will require more than just changing these classes, for example the settings page requires you to change the title to "Settings"
-   * - And each individual entry the title changes to "Entry #" based on it's number in the entry order
-   *
-   * - When changing states, make sure the back and forward buttons work. You can use hash URLs (e.g. https://someurl.com/#settings) when changing states
-   *   to make things easier.
-   * - Similarly, when viewing an individual entry, a hashed URL might look like https://someurl.com/#entry3
-   * 
-   * - Some tips:
-   *    1. Push a new state object to the history object using history.pushState() 
-   *    2. look up the documentation for how to use pushState() when you try it
-   *    3. look up the documentation for the "popstate" event listener (fires only on back button), useful in your script.js file
-   *    4. For each <journal-entry> element, you can grab the JSON version of its info with .entry (e.g. someJournalEntryElement.entry)
-   *       a. This is useful when viewing a single entry. You may notice an <entry-page> element in the HTML, this is the element that is displayed when the
-   *          .single-entry class is applied to the body. You can populate this element by using .entry similarly. So if I wanted to grab a specific <journal-entry>
-   *          and populate it's info into the <entry-page>, I would simply use an assignment of entryPageElement.entry = journalEntryElement.entry
-   *       b. Clearing the <entry-page> element of its previous data can be a bit tricky, it might be useful to just delete it and insert a new blank one 
-   *          in the same spot each time. Just a thought.
-   *
-   * - Answers to some questions you may have:
-   *    1. You may add as many helper functions in this file as you like
-   *    2. You may modify the parameters of setState() as much as you like
-   */
+router.setState = function(state, back) {
+  // go to Home page
+  if (state == null || state.name == 'home') {
+    body.className = ''; 
+    title.innerText = 'Journal Entries';
+
+    // add entry to browser's session history stack
+    if (back == false) {
+      history.pushState({page: state}, '', "./"); 
+    }
+  }
+
+  // go to Settings page
+  else if (state.name == 'settings') {
+    body.className = 'settings';
+    title.innerText = 'Settings'; 
+
+    // add entry to browser's session history stack
+    if (back == false) {
+      window.history.pushState({page: state}, '', '#settings');  
+    }
+  }
+
+  // go to Entry page
+  else if (state.name == 'entry') {
+    body.className = 'single-entry'; 
+    title.innerText = 'Entry ' + state.id;
+
+    let eachEntry = document.createElement('entry-page');
+    eachEntry.entry = document.getElementById(state.id).entry;
+    body.removeChild(document.getElementsByTagName("entry-page")[0]);
+    body.appendChild(eachEntry);
+
+    // add entry to browser's session history stack
+    if (back == false) {
+      history.pushState({page: state}, '', '#entry'.concat(state.id));
+    }
+  }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,20 @@
 import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
 const setState = router.setState;
 
+let entryNum = 0;
+
 // Make sure you register your service worker here too
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   fetch('https://cse110lab6.herokuapp.com/entries')
@@ -12,7 +25,30 @@ document.addEventListener('DOMContentLoaded', () => {
       entries.forEach(entry => {
         let newPost = document.createElement('journal-entry');
         newPost.entry = entry;
+
+        entryNum++;
+        newPost.id = entryNum;
+        newPost.addEventListener('click', () => {
+          setState({name: 'entry', id: newPost.id}, false);
+        });
+
         document.querySelector('main').appendChild(newPost);
       });
     });
+});
+
+document.querySelector('header h1').addEventListener('click', () => {
+if (history.state != null && history.state.name != 'home') {
+    setState({name: 'home'}, false);
+  };
+});
+    
+document.querySelector('header img').addEventListener('click', () => {
+  if (history.state == null || history.state.name != 'settings') {
+      setState({name: 'settings'}, false);
+  }
+});
+
+window.addEventListener('popstate', (e) => {
+  setState(e.state, true);
 });
